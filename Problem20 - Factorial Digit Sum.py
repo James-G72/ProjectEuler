e9b0b0bdd3dbfbe9dfb04b71e55db43d4b0cce4f
James Gower <66065325+James-G72@users.noreply.github.com>
2025-10-09 08:32:38 +0100

    Problem20 solved

---
+++ Problem20 - Factorial Digit Sum.py
@@ -0,0 +1,13 @@
+import GeneralFunctions as gf
+
+ANSWER = 648
+INPUT = 100
+
+
+def main(n):
+	return sum([int(x) for x in str(gf.factorial(n))])
+
+
+if __name__ == "__main__":
+	answer = main(INPUT)
+	print(f"The sum of the digits of {INPUT}! is {answer}")
