52ecbc7d6d54652d6715de6fc36082f3ce27b0a1
James Gower <66065325+James-G72@users.noreply.github.com>
2025-10-11 10:59:32 +0100

    Big speed-up for Problem 9 by ensuring b>a in the loop and calculating c instead of having another loop

--- Problem9 - Special Pythagorean Triplet.py
+++ Problem9 - Special Pythagorean Triplet.py
@@ -4,12 +4,12 @@ INPUT = 1000
 def main(n, print_ans=False):
     """Find the values of a,b and c, such that a^2+b^2+=c^2 and a+b+c=1000. Return a*b*c."""
     for a in range(1, n):
-        for b in range(1, n):
-            for c in range(1, n):
-                if a < b < c and a + b + c == n and a*a + b*b == c*c:
-                    if print_ans:
-                        print(f"a = {a}, b = {b}, c = {c}")
-                    return a*b*c
+        for b in range(a+1, n):
+            c = n - a - b
+            if a < b < c and a*a + b*b == c*c:
+                if print_ans:
+                    print(f"a = {a}, b = {b}, c = {c}")
+                return a*b*c
     return False
 
 if __name__ == "__main__":
