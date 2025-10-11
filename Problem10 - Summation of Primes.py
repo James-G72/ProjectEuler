47cf91edb532cf2ef5c9d0ed29f7e30054135d3a
James Gower <66065325+James-G72@users.noreply.github.com>
2025-10-11 13:33:37 +0100

    Implemented the sieve of eratosthenes to give me all primes as quickly as possible. Problem 10 is now 40 times faster

--- Problem10 - Summation of Primes.py
+++ Problem10 - Summation of Primes.py
@@ -4,14 +4,23 @@ ANSWER = 142913828922
 INPUT = 2000000
 
 
-def main(n):
-    """Sum all the prime number below a given value."""
-    prime_sum = 5
-    for check in range(5, n, 2):
-        if gf.check_prime(check):
-            prime_sum += check
+def sieve_of_eratosthenes(num):
+    sieve = [True] * num
+    for p in range(2, num):
+        if sieve[p]:
+            for multiple in range(p * p, num, p):
+                sieve[multiple] = False
+    # 1 is not prime
+    sieve[1] = False
+    # Throughout, index was treated as number, we therefore drop the first index 0
+    return sieve[1:]
 
-    return prime_sum
+
+def main(n):
+    """Sum all the prime numbers below a given value."""
+    prime_sieve = sieve_of_eratosthenes(n)
+    # As we now want to represent index as numbers we need to add one
+    return sum([idx+1 for idx, check in enumerate(prime_sieve) if check])
 
 
 if __name__ == "__main__":
